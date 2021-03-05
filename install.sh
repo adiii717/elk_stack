6c183abd23ddc3b59e7bb3e9ff49037ac0a863ef
Adil M <adilm717@gmail.com>
2021-03-05 10:33:07 +0500

    add metrics-beat for node metrics

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.1-x86_64.rpm
 yum install elasticsearch-7.10.1-x86_64.rpm  -y 
 sudo systemctl daemon-reload
@@ -12,5 +11,11 @@ sudo /bin/systemctl enable kibana.service
 wget https://raw.githubusercontent.com/Adiii717/elk_stack/main/config/kibana.yml /etc/kibana/kibana.yml
 sudo systemctl start kibana.service
 
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.10.1-x86_64.rpm
+sudo rpm -vi metricbeat-7.10.1-x86_64.rpm
+metricbeat modules list
+metricbeat modules enable elasticsearch-xpack
+systemctl enable metricbeat
+systemctl start metricbeat
 
 
